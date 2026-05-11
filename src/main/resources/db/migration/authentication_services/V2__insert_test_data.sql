7b4db0d17cfe9e3a522910285ba871f50b563304
lucas <lucas.devtibetano@gmail.com>
2026-05-11 11:53:25 -0300

    Add example data file.

--- src/main/resources/db/migration/authentication_services/V2__insert_test_data.sql
+++ src/main/resources/db/migration/authentication_services/V2__insert_test_data.sql
@@ -3,15 +3,15 @@ VALUES ('88662959-7fe3-4687-92b9-7a587381466c', 2026, 3)
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO ticket_services.ticket_batch (id,event_id,name,start_at,end_at,status)
-VALUES ('4a9fbad4-5d8f-43e3-914e-38ba7e53eb75','88662959-7fe3-4687-92b9-7a587381466c','lote_de_abertura','2026-4-4','2026-6-6','ACTIVE')
+VALUES ('4a9fbad4-5d8f-43e3-914e-38ba7e53eb75','88662959-7fe3-4687-92b9-7a587381466c','01','2026-4-4','2026-6-6','ACTIVE')
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO ticket_services.ticket_batch_type (id,ticket_batch_id,ticket_type,price,total_quantity,sold_quantity,reserved_quantity)
-VALUES ('5ca392a8-c3fc-423d-8d78-d8acd59452bf','4a9fbad4-5d8f-43e3-914e-38ba7e53eb75','FULL',3000,5,0,0)
+VALUES ('5ca392a8-c3fc-423d-8d78-d8acd59452bf','4a9fbad4-5d8f-43e3-914e-38ba7e53eb75','FULL',600,15,0,0)
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO ticket_services.ticket_batch_type (id,ticket_batch_id,ticket_type,price,total_quantity,sold_quantity,reserved_quantity)
-VALUES ('c32f4e7b-ecfd-4ef8-9832-f1f697915be8','4a9fbad4-5d8f-43e3-914e-38ba7e53eb75','HALF',3000,5,0,0)
+VALUES ('c32f4e7b-ecfd-4ef8-9832-f1f697915be8','4a9fbad4-5d8f-43e3-914e-38ba7e53eb75','HALF',300,5,0,0)
 ON CONFLICT (id) DO NOTHING;
 
 -->pedidos
